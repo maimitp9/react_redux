cb4a2c0c9b4f264ae10d7b72b4c1fbf28c7d7e5f
Maimit Patel <maimitp9@gmail.com>
2017-09-01 00:11:42 +0530

    add material ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Routes from '../Routes/Routes';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Header from '../layout/Header';
 import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
+      <div>
+        <MuiThemeProvider>
+          <Header />
+        </MuiThemeProvider>
         <Routes />
       </div>
     );
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { AppBar, Drawer, MenuItem, IconButton } from 'material-ui';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
 class Header extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle(){
+     this.setState({open: !this.state.open})
+  }
   render(){
     return(
       <div>
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
+      <AppBar title="React Redux Demo" onLeftIconButtonTouchTap={this.handleToggle.bind(this)}></AppBar>
+        <Drawer open={this.state.open}>
+            <AppBar iconElementRight={<IconButton><NavigationClose /></IconButton>} onRightIconButtonTouchTap={this.handleToggle.bind(this)} />
+            <MenuItem><Link to="/" className="link"> Home </Link></MenuItem>
+            <MenuItem><Link to="/users">Users</Link></MenuItem>
+        </Drawer>
+
+
       </div>
     )
   }
