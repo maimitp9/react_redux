140a25c9b102360a8e080a206fae2a7accd50d46
Maimit <maimit@thinkbiz.co.in>
2017-08-31 17:46:53 +0530

    remove static json

--- src/actions/action_users.js
+++ src/actions/action_users.js
@@ -1,15 +1,5 @@
 import axios from 'axios';
 
-export const showUser = (user_id) =>{
-  const  user = user_data.find(function(user){
-    return user.id === +user_id
-  })
-  return {
-    type: "SHOW_USER",
-    payload: user
-  }
-}
-
 // fetch users
 export const fetchUsers = () =>{
   const request = axios({
@@ -67,21 +57,3 @@ export function resetActiveUser(){
     type: "RESET_ACTIVE_USER"
   }
 }
-
-const user_data = [
-    {
-      id: 1,
-      fname: "Maimit",
-      lname: "Patel"
-    },
-    {
-      id: 2,
-      fname: "Chintu",
-      lname: "Patel"
-    },
-    {
-      id: 3,
-      fname: "Pihu",
-      lname: "Patel"
-    }
-  ]
